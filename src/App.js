50df6e819ef9144633ff4ec05dd0acc4c2089d57
mj305 <maria.j.beckles@gmail.com>
2020-05-27 11:52:04 -0400

    showing class types

--- src/App.js
+++ src/App.js
@@ -18,18 +18,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
           {classes.map(classRoom => {
         return <p key={classRoom.type}>{classRoom.type}</p>
+        
       })}
-
-
-        </a>
       </header>
   
     </div>
