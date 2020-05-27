d599dc73e1177968eeae8056d53aa751918c8374
mj305 <maria.j.beckles@gmail.com>
2020-05-27 10:42:44 -0400

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [classes, setClasses] = useState([])
+  useEffect(() => {
+    fetch(`http://localhost:4000/classes`)
+    .then(response => response.json())
+    .then(
+      classRoom => {
+        setClasses(classRoom);
+      }
+    )
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {classes.map(classRoom => {
+        return <p key={classRoom.type}>{classRoom.type}</p>
+      })}
+
+
         </a>
       </header>
+  
     </div>
   );
 }
