41cf797a5724701fabc0e6cec2792f4cadfe0c81
Sonia <st.remysonia@gmail.com>
2020-08-04 19:11:20 -0400

    added padding to the links

--- src/Components/NavBarFolder/NavBar.js
+++ src/Components/NavBarFolder/NavBar.js
@@ -5,16 +5,17 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
-
 import './NavBar.css';
 
 const NavBar = () => {
   return (
     <>
     <Router>
-      <Link className="" to="/sign in">Log In</Link>
-      <Link className="" to="/sign in"> Sign Up</Link>
+      <div className="navbar-container">
+        <Link className="navbar-logo" to="#">Logo</Link>
+        <Link className="navbar-log-in navbar" to="/log in">Log In</Link>
+        <Link className="navbar-sign-up navbar" to="/sign up"> Sign Up</Link>
+      </div>
     </Router>    
     </>
   )
