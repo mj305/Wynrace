3a2ebc61a8bde254536a848b2bf52472ab046e17
Sonia <st.remysonia@gmail.com>
2020-08-04 15:50:14 -0400

    fix conflict

--- src/Components/NavBarFolder/NavBar.js
+++ src/Components/NavBarFolder/NavBar.js
@@ -6,8 +6,11 @@ const NavBar = () => {
   return (
     <>
       <div className="navbar">
-        <nav><a id="log-in" className="btn" href="log in">Log In</a></nav>
-        <nav><a id="sign-up" className="btn" href="sign up">Sign Up</a></nav>
+        
+        <nav>
+          <a id="log-in" className="btn" href="log in">Log In</a>
+          <a id="sign-up" className="btn" href="sign up">Sign Up</a>
+        </nav>
       </div>
     </>
   )
