4712d1f5d6059d1ec1f2a9f724032534333336d9
mj305 <maria.j.beckles@gmail.com>
2020-08-04 16:19:03 -0400

    added Sonia NavBar

--- src/Components/NavBarFolder/NavBar.js
+++ src/Components/NavBarFolder/NavBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+import './NavBar.css';
 
 const NavBar = () => {
-
   return (
     <>
-    <h2> NavBar </h2>
+    <Router>
+          <Link className="" to="/sign in">Log In</Link>
+          <Link className="" to="/sign in"> Sign Up</Link>
+          </Router>    
     </>
   )
 }
-
 export default NavBar;
